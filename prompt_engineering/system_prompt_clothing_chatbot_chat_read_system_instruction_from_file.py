13d675abfbc07c1bf62172116e7426d1a583dded
Clara <10088152+claraj@users.noreply.github.com>
2025-09-27 20:43:58 -0500

    system instructions from file

---
+++ prompt_engineering/system_prompt_clothing_chatbot_chat_read_system_instruction_from_file.py
@@ -0,0 +1,33 @@
+import os 
+import sys 
+from google import genai 
+from google.genai.types import GenerateContentConfig
+import rich 
+from rich.markdown import Markdown
+
+system_instruction_file_path = os.path.join('system_instructions', 'clothing_chatbot.txt')
+
+try: 
+    with open(system_instruction_file_path, 'r') as file:
+        system_instruction = file.read()
+except Exception as e:
+    print('System instructions not found')
+    sys.exit()
+
+client = genai.Client()
+chat = client.chats.create(
+    model='gemini-2.5-flash',
+    config=GenerateContentConfig(
+        system_instruction=system_instruction
+        )
+    )
+
+print('This is Betty with Zoomies! How can I help you?')
+while True:
+    prompt = input('✨> ')
+    response = chat.send_message(prompt)
+    rich.print(Markdown(response.text))
+
+
+
+
